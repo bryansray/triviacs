6803d49f66f2b29b55774cff0fb238f46d6589cc
Bryan Ray <bryan@bryanray.net>
2019-05-18 10:27:50 -0500

    Working on endpoint to return resource of answers.

--- Trivia.API/Controllers/Questions/AnswersController.cs
+++ Trivia.API/Controllers/Questions/AnswersController.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Trivia.API.Common;
+using Trivia.API.Common.Web;
 
 namespace Trivia.API.Controllers.Questions
 {
@@ -10,29 +14,36 @@ namespace Trivia.API.Controllers.Questions
     [ApiController]
     public class AnswersController : ControllerBase
     {
-        private readonly TriviaDbContext _context;
+        private readonly IDbConnection _connection;
 
-        public AnswersController(TriviaDbContext context)
+        public AnswersController(IDbConnection connection)
         {
-            _context = context;
+            _connection = connection;
         }
         
         [HttpGet]
         public async Task<ActionResult> Index([FromRoute] int questionId)
         {
-            var question = await _context.Questions.SingleOrDefaultAsync(x => x.Id == questionId);
-            var answers = await _context.Answers
-                .Include(x => x.Question)
-                .Where(x => x.Question.Id == questionId)
-                .OrderByDescending(x => x.Count)
-                .ToListAsync();
-
-            var result = new
+            var dictionary = new Dictionary<int, Question>();
+            
+            Question MapResults(Question q, Answer a)
             {
-                Question = Mapper.MapQuestionToResourceModel(question),
-                Answers = answers.Select(Mapper.MapAnswerToResourceModel)
-            };
-            return Ok(result);
+                if (!dictionary.TryGetValue(q.Id, out var entry))
+                {
+                    entry = q;
+                    entry.Answers = new List<Answer>();
+                    dictionary.Add(entry.Id, entry);
+                }
+
+                entry.Answers.Add(a);
+                return entry;
+            }
+            
+            var question = (await _connection.QueryAsync<Question, Answer, Question>(
+                "SELECT * FROM questions INNER JOIN answers ON questions.id = answers.question_id WHERE questions.id = @questionId",
+                MapResults, new {questionId})).Distinct().SingleOrDefault();
+            if (question == null) return NotFound();
+            return Ok(new SuccessEnvelope(Mapper.MapQuestionToResourceModel(question)));
         }
     }
 }
